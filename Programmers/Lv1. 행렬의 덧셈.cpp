7b334eba22ef81d89a83818f08aad0f86beec5a3
Kim KunWoo <rlarjsdn3@naver.com>
2022-09-25 16:06:01 +0900

    Update Lv1. 행렬의 덧셈.cpp

--- Programmers/Lv1. 행렬의 덧셈.cpp
+++ Programmers/Lv1. 행렬의 덧셈.cpp
@@ -18,3 +18,17 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 
     return answer;
 }
+
+int main(void) {
+    vector<vector<int>> arr1{ { 1, 2 }, { 3, 4} }, arr2{ { 3, 4}, { 5, 6 } };
+    vector<vector<int>> answer = solution(arr1, arr2);
+
+    for(int i=0; i<answer.size(); i++) {
+        for(int j=0; j<answer[i].size(); j++) {
+            cout << answer[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
