7c469136af35af6f6a3ee039927ce42e6d052138
Kim KunWoo <rlarjsdn3@naver.com>
2022-09-26 22:46:13 +0900

    Update 2480: 주사위 세개.java

--- BaekJoon/2480: 주사위 세개.java
+++ BaekJoon/2480: 주사위 세개.java
@@ -7,15 +7,18 @@ public class main {
         int a = Integer.parseInt(sc.next());
         int b = Integer.parseInt(sc.next());
         int c = Integer.parseInt(sc.next());
+        int result = 0;
 
         if(a==b && b==c && a==c) {
-            System.out.println(10000+(a*1000));
+            result = 10000+(a*1000);
         } else if(a==b || b==c) {
-            System.out.println(1000+(b*100));
+            result = 1000+(b*100);
         } else if(a==c) {
-            System.out.println(1000+(c*100));
+            result = 1000+(c*100);
         } else {
-            System.out.println((a>b?(a>c?a:c):(b>c?b:c))*100);
+            result = (a>b?(a>c?a:c):(b>c?b:c))*100;
         }
+
+        System.out.println(result);
     }
 }
