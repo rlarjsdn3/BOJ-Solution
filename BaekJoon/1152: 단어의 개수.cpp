53700a8c69139a313d1dd68d4633b684ecca1cac
Kim KunWoo <rlarjsdn3@naver.com>
2022-09-27 09:39:05 +0900

    Create 1152: 단어의 개수.cpp

---
+++ BaekJoon/1152: 단어의 개수.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sstream>
+#include <string> 
+#include <vector>
+
+std::vector<std::string> split(std::string str, char delimeter) {
+    std::string buff;
+    std::vector<std::string> tmp;
+    std::istringstream iss(str);
+
+    while(std::getline(iss, buff, delimeter)) {
+        if(buff!="") tmp.push_back(buff);
+    }
+    
+    return tmp;
+}
+
+int main(void) {
+    std::vector<std::string> vector;
+    std::string str = "a b c";
+
+    std::getline(std::cin, str);
+    vector = split(str, ' ');
+    std::cout << vector.size() << std::endl;
+
+    return 0;
+}
