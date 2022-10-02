531caef8b7a85319bc4105e3e4d68489993655de
Kim KunWoo <rlarjsdn3@naver.com>
2022-10-02 19:25:43 +0900

    Create 14720: 우유 축제.py

---
+++ BaekJoon/14720: 우유 축제.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = list(map(int, input().split()))
+
+count = 0
+next_milk = 0
+
+for i in range(0, len(m)):
+    if m[i]==next_milk:
+        next_milk = ((next_milk+1)%3)
+        count += 1
+
+print(count)
