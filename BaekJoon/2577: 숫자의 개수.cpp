626a803ebceb11cc40a78ed91919452552742c36
Kim KunWoo <rlarjsdn3@naver.com>
2022-10-03 18:58:49 +0900

    Create 2577: 숫자의 개수.cpp

---
+++ BaekJoon/2577: 숫자의 개수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+
+int main(void)
+{
+    int a, b, c;
+    int count[10] = { 0 };
+    std::string mul;
+
+    std::cin >> a >> b >> c;
+    mul = std::to_string(a*b*c);
+
+    for(auto it=mul.begin(); it!=mul.end(); it++) {
+        count[(int)(*it-'0')] += 1;
+    }
+
+    for(int i=0; i<10; i++) {
+        std::cout << count[i] << '\n';
+    }
+
+    return 0;
+}
