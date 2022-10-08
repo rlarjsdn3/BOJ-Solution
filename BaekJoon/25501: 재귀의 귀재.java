47ad5386d959ec92d1ea639ded0f848bbdb3197a
Kim KunWoo <rlarjsdn3@naver.com>
2022-10-08 16:08:26 +0900

    Create 25501: 재귀의 귀재.java

---
+++ BaekJoon/25501: 재귀의 귀재.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class main{
+    static int count = 1;
+
+    public static int recursion(String s, int l, int r){
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else {
+            count += 1;
+            return recursion(s, l+1, r-1); 
+        }
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        String[] s = new String[n];
+        for(int i=0; i<n; i++) {
+            s[i] = sc.next();
+        }
+
+        for(int i=0; i<n; i++) {
+            System.out.println(isPalindrome(s[i]) + " " + count);
+            count = 1;
+        }
+    }
+}
