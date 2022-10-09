f89bbd445c5136c57cd8fcfec89a805b7785bc2c
Kim KunWoo <rlarjsdn3@naver.com>
2022-10-09 22:23:29 +0900

    Create 11659: 구간 합 구하기 4.java

---
+++ BaekJoon/11659: 구간 합 구하기 4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int valueSum = 0;
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[] data = new int[n];
+        int[] prefixSum = new int[n+1];
+        int[] result = new int[m];
+        for(int i=0; i<n; i++) {
+            data[i] = sc.nextInt();
+            valueSum += data[i];
+            prefixSum[i+1] = valueSum;
+        }
+
+        for(int i=0; i<m; i++) {
+            int left = sc.nextInt(), right = sc.nextInt();
+            result[i] = prefixSum[right] - prefixSum[left-1];
+        }
+
+        Arrays.stream(result).forEach(System.out::println);
+    }
+}
