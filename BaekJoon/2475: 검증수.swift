1b302d1cefe5dbfbf1927911e38c416f3b2d0481
Zizi_Kim <rlarjsdn3@naver.com>
2022-10-30 00:32:59 +0900

    Create 2475: 검증수.swift

---
+++ BaekJoon/2475: 검증수.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let n: [Int] = Array(readLine()!.components(separatedBy: " ")).map { Int($0)! }
+
+var sum: Int = 0
+for x in n {
+    sum += Int(pow(Double(x), 2.0))
+}
+
+print(sum % 10)
