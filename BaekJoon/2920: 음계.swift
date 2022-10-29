afb1d547db73b630f27fb639782252cde3e1bb25
Zizi_Kim <rlarjsdn3@naver.com>
2022-10-30 00:19:54 +0900

    Create 2920: 음계.swift

---
+++ BaekJoon/2920: 음계.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let n: [Int] = Array(readLine()!.components(separatedBy: " ")).map { Int($0)! }
+
+var isAscending: Bool = false
+var isDescending: Bool = false
+
+for i in 1..<n.count {
+    if n[i-1] < n[i] {
+        isAscending = true
+    } else {
+        isDescending = true
+    }
+}
+
+print((isAscending && isDescending) ? "mixed" : (isAscending ? "ascending" : "descending"))
