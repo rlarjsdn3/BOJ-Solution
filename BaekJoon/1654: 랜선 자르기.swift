964bb2dfb7827bf699891c38b4ecfb411152b499
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-07 23:23:48 +0900

    Create 1654: 랜선 자르기.swift

---
+++ BaekJoon/1654: 랜선 자르기.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+let k: Int = input[0]
+let n: Int = input[1]
+var lanCable: [Int] = []
+for _ in 0..<k {
+    lanCable.append(Int(readLine()!)!)
+}
+lanCable.sort()
+
+var start: Int = 1
+var end: Int = lanCable[lanCable.count-1]
+
+var result: Int = 0
+while start <= end {
+    let mid: Int = (start+end) / 2
+    
+    var count: Int = 0
+    for i in lanCable {
+        count += (i / mid)
+    }
+    
+    if n <= count {
+        start = mid + 1
+        result = start
+    } else {
+        end = mid - 1
+    }
+}
+
+print(result - 1)
