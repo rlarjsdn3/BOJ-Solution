dfc933f9e1bd2544ebd14df614afa03b47b67e3a
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-14 13:26:24 +0900

    Create 10162: 전자레인지.swift

---
+++ BaekJoon/10162: 전자레인지.swift
@@ -0,0 +1,20 @@
+//
+//  Created by 김건우 on 2022/11/13.
+//
+
+import Foundation
+
+var input: Int = Int(readLine()!)!
+var button: [Int] = [300, 60, 10]
+var result: [Int] = [0, 0, 0]
+
+for i in button.indices {
+    result[i] += (input / button[i])
+    input -= (button[i] * result[i])
+}
+
+if input == 0 {
+    result.forEach { print($0, terminator: " ") }
+} else {
+    print("-1")
+}
