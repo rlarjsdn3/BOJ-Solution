ac1cca36a4e2748fcf025345b311822fbbe42af5
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-15 00:55:42 +0900

    Create 2609: 최대공약수와 최소공배수.swift

---
+++ BaekJoon/2609: 최대공약수와 최소공배수.swift
@@ -0,0 +1,15 @@
+//
+//  Created by 김건우 on 2022/11/15.
+//
+
+import Foundation
+
+func getGcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 { return a }
+    else { return getGcd(b, a%b) }
+}
+
+let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+let gcd: Int = getGcd(input[0], input[1])
+let lcm: Int = gcd * (input[0]/gcd) * (input[1]/gcd)
+print("\(gcd) \(lcm)")
