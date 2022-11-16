aca4017e157320e3c2458804a8972ce339f74b17
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-16 18:56:05 +0900

    Create 11866: 요세푸스 문제 0.swift

---
+++ BaekJoon/11866: 요세푸스 문제 0.swift
@@ -0,0 +1,45 @@
+//
+//  Created by 김건우 on 2022/11/16.
+//
+
+
+import Foundation
+
+struct Queue<T> {
+    private var items: [T] = []
+    var isEmpty: Bool {
+        return items.isEmpty
+    }
+    
+    mutating func push(_ element: T) {
+        items.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        if isEmpty {
+            return nil
+        }
+        
+        return items.removeFirst()
+    }
+}
+
+let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n: Int = input[0]
+let k: Int = input[1]
+var queue: Queue<Int> = Queue<Int>()
+for i in 1...n {
+    queue.push(i)
+}
+
+var result: [String] = []
+while !queue.isEmpty {
+    for _ in 0..<(k-1) {
+        if let q = queue.pop() {
+            queue.push(q)
+        }
+    }
+    
+    result.append(String(queue.pop()!))
+}
+print("<" + result.joined(separator: ", ") + ">")
