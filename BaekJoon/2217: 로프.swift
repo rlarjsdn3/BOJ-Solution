65861ac0eec7e6c1158c9b8a9ed4099d400df355
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-16 18:20:09 +0900

    Create 2217: 로프.swift

---
+++ BaekJoon/2217: 로프.swift
@@ -0,0 +1,22 @@
+//
+//  Created by 김건우 on 2022/11/16.
+//
+
+
+import Foundation
+
+let n: Int = Int(readLine()!)!
+var rope: [Int] = []
+for _ in 0..<n {
+    rope.append(Int(readLine()!)!)
+}
+rope.sort(by: >)
+
+var max: Int = rope[0]
+for i in 1..<rope.count {
+    let tmp: Int = rope[i] * (i+1)
+    if max < tmp {
+        max = tmp
+    } 
+}
+print(max)
