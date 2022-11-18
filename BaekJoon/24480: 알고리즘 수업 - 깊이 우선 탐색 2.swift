810366df8eea3054f08bb6024c47630a245bc67d
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-18 16:40:41 +0900

    Create 24480: 알고리즘 수업 - 깊이 우선 탐색 2.swift

---
+++ BaekJoon/24480: 알고리즘 수업 - 깊이 우선 탐색 2.swift
@@ -0,0 +1,56 @@
+//
+// Created by 김건우 on 2022/11/18.
+//
+
+import Foundation
+
+var count: Int = 1
+func dfs(_ graph: [[Int]], v: Int, visited: inout [Int]) {
+    visited[v] = count
+    
+    for i in graph[v] {
+        if visited[i] == 0 {
+            count += 1
+            dfs(graph, v: i, visited: &visited)
+        }
+    }
+}
+
+let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n: Int = input[0]
+let m: Int = input[1]
+let r: Int = input[2]
+
+var graph: [[Int]] = [[Int]](repeating: [Int](), count: n+1)
+for _ in 0..<m {
+    let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let u: Int = input[0]
+    let v: Int = input[1]
+    graph[u].append(v)
+    graph[v].append(u)
+}
+for i in graph.indices {
+    graph[i].sort(by: >)
+}
+
+var visited: [Int] = [Int](repeating: 0, count: n+1)
+
+dfs(graph, v: r, visited: &visited)
+for i in 1..<visited.count {
+    print(visited[i])
+}
+
+// input:
+// 5 5 1
+// 1 4
+// 1 2
+// 2 3
+// 2 4
+// 3 4
+
+// output:
+// 1
+// 4
+// 3
+// 2
+// 0
