4a21f455fb52d45cf773067288a13177545a8e7b
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-23 18:34:45 +0900

    Update 2606_바이러스.swift

--- BaekJoon/2606_바이러스.swift
+++ BaekJoon/2606_바이러스.swift
@@ -1,31 +1,45 @@
 import Foundation
 
-// 깊이 우선 탐색 구현 함수
-func dfs(_ graph: [[Int]], v: Int, visited: inout [Bool]) {
-    visited[v] = true
-    
-    for i in graph[v] {
-        if !visited[i] {
-            dfs(graph, v: i, visited: &visited)
-        }
-    }
-}
+// 정점의 수, 간선의 수 입력받기
+let n: Int = Int(readLine()!)!
+let m: Int = Int(readLine()!)!
+var graph: [[Int]] = [[Int]](repeating: [Int](), count: n+1);
 
-let n: Int = Int(readLine()!)! // 컴퓨터의 수
-let m: Int = Int(readLine()!)! // 연결된 간선의 수
-var graph: [[Int]] = [[Int]](repeating: [Int](), count: n+1)
+// 간선을 연결 리스트 형태로 입력받기
 for _ in 0..<m {
-    // 그래프를 연결된 리스트 형테로 입력 받기
     let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
-    let u: Int = input[0]
-    let v: Int = input[1]
+    let (u, v) = (input[0], input[1])
     graph[u].append(v)
     graph[v].append(u)
 }
 
-// 해당 노드를 방문했는지 체크하는 변수
-var visited: [Bool] = [Bool](repeating: false, count: n+1)
+// 오름차순으로 탐색해야 하므로 정렬
+for i in 1...n {
+    graph[i].sort()
+}
+var visited: [Bool] = [Bool](repeating: false, count: n+1) // 방문한 노드인지 확인하는 배열
 
-// 시작점을 1로 하여 깊이 우선 탐색 수행
-dfs(graph, v: 1, visited: &visited)
-print(visited.filter { $0 == true }.count - 1) // visited 배열 중에서 true인 값만 세어 출력하기
+var count: Int = 1
+func dfs(_ graph: [[Int]], vertex: Int, visited: inout [Bool]) {
+    // 해당 노드에 방문했다고 표시
+    visited[vertex] = true
+    
+    for i in graph[vertex] {
+        // 해당 노드를 방문하지 않았으면
+        if !visited[i] {
+            // 방문하기
+            dfs(graph, vertex: i, visited: &visited)
+        }
+    }
+}
+
+dfs(graph, vertex: 1, visited: &visited) // 깊이 우선 탐색 수행
+
+// 1번 컴퓨터를 제외한 윔 바이러스에 걸린 컴퓨터의 수 계산 후 출력하기
+var result: Int = 0
+for i in 1...n {
+    if visited[i] {
+        result += 1
+    }
+}
+print(result - 1)
