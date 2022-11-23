4be804d0041db6907673e0c6b45db99046f8ef77
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-23 19:08:03 +0900

    Update 4963_섬의 개수.swift

--- BaekJoon/4963_섬의 개수.swift
+++ BaekJoon/4963_섬의 개수.swift
@@ -2,24 +2,24 @@ import Foundation
 
 @discardableResult
 func dfs(_ graph: inout [[Int]], x: Int, y: Int) -> Bool {
-    // 시작점(x, y)가 범위를 벗어나는 경우 false 리턴
+    // 좌표가 지도 크기의 범위를 넘어서면 false 반환
     if x < 0 || x >= graph.count || y < 0 || y >= graph[0].count {
         return false
     }
     
-    // 시작점(x, y)을 아직 방문하지 않은 경우
+    // 해당 노드에 방문하지 않았으면
     if graph[x][y] == 1 {
-        // 시작점 방문 처리
+        // 해당 노드 방문 처리
         graph[x][y] = 0
         
-        // 각 좌표점으로 육지가 존재하는지 확인
-        dfs(&graph, x: x + 1, y: y)
-        dfs(&graph, x: x - 1, y: y)
+        // 가로, 세로와 대각선으로 순회
         dfs(&graph, x: x, y: y + 1)
         dfs(&graph, x: x, y: y - 1)
-        dfs(&graph, x: x + 1, y: y + 1)
+        dfs(&graph, x: x + 1, y: y)
+        dfs(&graph, x: x - 1, y: y)
         dfs(&graph, x: x + 1, y: y - 1)
         dfs(&graph, x: x - 1, y: y + 1)
+        dfs(&graph, x: x + 1, y: y + 1)
         dfs(&graph, x: x - 1, y: y - 1)
         return true
     }
@@ -28,26 +28,27 @@ func dfs(_ graph: inout [[Int]], x: Int, y: Int) -> Bool {
 }
 
 var result: [Int] = []
+var visited: [Bool] = []
 while true {
+    // W와 H 입력 받기
     let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
-    let w: Int = input[0]
-    let h: Int = input[1]
-    
-    // W와 H가 0이 입력되면 while문 탈출
-    if w == 0 && h == 0 {
+    let (w, h) = (input[0], input[1])
+    // W와 H를 0으로 입력받으면 루프 탈출
+    guard w != 0 && h != 0 else {
         break
     }
     
+    // 간선 정보 입력 받기
     var graph: [[Int]] = []
     for _ in 0..<h {
         graph.append(readLine()!.split(separator: " ").map { Int(String($0))! })
     }
     
-    // 모든 시작점을 기준으로
-    // 하나의 이어지는 육지가 존재하면 count 증가
+    // 섬의 개수를 세어 결과 저장
     var count: Int = 0
     for i in 0..<h {
         for j in 0..<w {
+            // 섬이 존재하면 count 증가
             if dfs(&graph, x: i, y: j) {
                 count += 1
             }
@@ -56,4 +57,6 @@ while true {
     
     result.append(count)
 }
+
+// 결과 출력
 result.forEach { print($0) }
