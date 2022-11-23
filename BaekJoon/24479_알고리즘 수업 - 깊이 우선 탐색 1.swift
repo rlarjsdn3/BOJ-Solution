483f46f1f6d2952e6600a2f47d4ff264fdf676ba
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-23 18:27:38 +0900

    Update 24479_알고리즘 수업 - 깊이 우선 탐색 1.swift

--- BaekJoon/24479_알고리즘 수업 - 깊이 우선 탐색 1.swift
+++ BaekJoon/24479_알고리즘 수업 - 깊이 우선 탐색 1.swift
@@ -1,41 +1,41 @@
-//
-//  Created by 김건우 on 2022/11/18.
-//
-
 import Foundation
 
+let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (n, m, r) = (input[0], input[1], input[2]) // 정점의 수, 간선의 수, 시작 정점 입력받기
+var graph: [[Int]] = [[Int]](repeating: [Int](), count: n+1);
+
+// 간선을 연결 리스트 형태로 입력받기
+for _ in 0..<m {
+    let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let (u, v) = (input[0], input[1])
+    graph[u].append(v)
+    graph[v].append(u)
+}
+
+// 오름차순으로 탐색해야 하므로 정렬
+for i in 1...n {
+    graph[i].sort()
+}
+var visited: [Int] = [Int](repeating: 0, count: n+1) // 방문한 노드인지 확인하는 배열
+
 var count: Int = 1
-func dfs(_ graph: [[Int]], v: Int, visited: inout [Int]) {
-    visited[v] = count
+func dfs(_ graph: [[Int]], vertex: Int, visited: inout [Int]) {
+    // 해당 노드에 방문했다고 표시 (방문 순서 기록하기)
+    visited[vertex] = count
     
-    for i in graph[v] {
+    for i in graph[vertex] {
+        // 해당 노드를 방문하지 않았으면
         if visited[i] == 0 {
+            // 방문하고 순서 매기기
             count += 1
-            dfs(graph, v: i, visited: &visited)
+            dfs(graph, vertex: i, visited: &visited)
         }
     }
 }
 
-let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
-let n: Int = input[0]
-let m: Int = input[1]
-let r: Int = input[2]
+dfs(graph, vertex: r, visited: &visited) // 깊이 우선 탐색 수행
 
-var graph: [[Int]] = [[Int]](repeating: [Int](), count: n+1)
-for _ in 0..<m {
-    let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
-    let u: Int = input[0]
-    let v: Int = input[1]
-    graph[u].append(v)
-    graph[v].append(u)
-}
-for i in graph.indices {
-    graph[i].sort()
-}
-
-var visited: [Int] = [Int](repeating: 0, count: n+1)
-
-dfs(graph, v: r, visited: &visited)
-for i in 1..<visited.count {
+// 결과 출력
+for i in 1...n {
     print(visited[i])
 }
