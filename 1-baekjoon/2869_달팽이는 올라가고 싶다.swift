593afbda4042f789f70ca645bf15e118e46b58d3
Zizi_Kim <rlarjsdn3@naver.com>
2023-01-01 21:33:17 +0900

    백준 2869번 문제_달팽이는 올라가고 싶다.swift 주석 수정

--- 1-baekjoon/2869_달팽이는 올라가고 싶다.swift
+++ 1-baekjoon/2869_달팽이는 올라가고 싶다.swift
@@ -5,7 +5,7 @@ let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
 let (a, b, v): (Int, Int, Int) = (input[0], input[1], input[2])
 
 var result: Int = (v - b) / (a - b)
-if (v - b) > (result * (a-b)) { // 한 번에 올라갈 수 있는 경우
+if (v - b) != (result * (a-b)) { // 마지막으로 올라가면 되는 경우
     result += 1
 }
 print(result) // 결과값 출력하기
